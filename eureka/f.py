37c8a6ac90ecba0698432925b549f99027fba09c
Kate Lawrence-Gupta <lokispundit@gmail.com>
2020-04-01 10:54:49 -0400

    Update f.py

--- eureka/f.py
+++ eureka/f.py
@@ -9,8 +9,8 @@ import gzip
 
 s3_resource = boto3.resource('s3')
 s3 = boto3.resource('s3',
-        aws_access_key_id=ACCESS_KEY,
-         aws_secret_access_key= SECRET_KEY)
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
 
 name = "klgdiag"
 new_name = "klgdiagunpacked"
