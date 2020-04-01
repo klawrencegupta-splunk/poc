afcf8664adbbe56a0364e12249a3445c10662c05
Kate Lawrence-Gupta <lokispundit@gmail.com>
2020-04-01 13:17:07 -0400

    Update f.py

--- eureka/f.py
+++ eureka/f.py
@@ -40,7 +40,7 @@ def s3_unpack(new_name,NEW_BUCKET):
     try:
         obj = s3.Object(new_name,key_name)
         #n = obj.get()['Body'].read()
-        n = obj.get()['ContentType']
+        n = obj.get()['ContentDisposition']
         print(n)
         #gzipfile = BytesIO(n)
         #gzipfile = gzip.GzipFile(fileobj=gzipfile)
