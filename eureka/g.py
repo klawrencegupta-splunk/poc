37825af4d9f8c5bd722234be122dbee6608dcaab
Kate Lawrence-Gupta <lokispundit@gmail.com>
2020-04-02 14:24:09 -0400

    Update g.py

--- eureka/g.py
+++ eureka/g.py
@@ -53,9 +53,9 @@ def s3_unpack(new_name,NEW_BUCKET):
         names = tarf.getnames() 
         for name in names:
             name=str(name)
-            res = [ele for ele in listOflogs if(ele in name)]
-            print res
-            print(name)     
+            for x in listOflogs:
+                if x in name:
+                   print(name)     
     except Exception as e:
         raise
     
