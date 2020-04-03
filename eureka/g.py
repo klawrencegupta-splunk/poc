c2a99b16f600d1cd2ad6938cb210a35cd96c7721
Kate Lawrence-Gupta <lokispundit@gmail.com>
2020-04-03 11:20:49 -0400

    Update g.py

--- eureka/g.py
+++ eureka/g.py
@@ -42,7 +42,7 @@ def s3_copy_diag(BUCKET,NEW_BUCKET):
 
 def s3_copy_diag_files(files_needed, NEW_BUCKET):
     for x in files_needed:
-        with tarfile.open(x, 'wb') as f:
+        with tarfile.open(x) as f:
              y=f.read()
              y=str(y)
              s3.upload_fileobj(f, new_name, y)          
